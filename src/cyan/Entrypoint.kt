9845fd86168493bf196acfe5980e70fc3868baf8
Benjamin Dupont <benjamin.dupont.984@gmail.com>
2020-07-28 00:46:18 -0400

    refactor: clean up entrypoint

--- src/cyan/Entrypoint.kt
+++ src/cyan/Entrypoint.kt
@@ -1,35 +1,22 @@
 package cyan
 
 import cyan.compiler.parser.CyanSourceParser
-import cyan.compiler.parser.ast.CyanSource
-import cyan.compiler.parser.ast.expression.literal.CyanStringLiteralExpression
 import cyan.compiler.fir.FirDocument
-import cyan.compiler.fir.expression.FirExpression
 import cyan.compiler.fir.functions.FirFunctionDeclaration
 import cyan.compiler.lower.ast2fir.SourceLower
 import cyan.compiler.codegen.js.JsCompilerBackend
-import cyan.compiler.codegen.wasm.WasmCompilerBackend
-import cyan.interpreter.Interpreter
 
 import com.github.h0tk3y.betterParse.grammar.parseToEnd
 
 import java.io.File
 
-import kotlin.time.ExperimentalTime
-import kotlin.time.measureTime
-
-@ExperimentalTime
 fun main() {
-//    println("welcome to cyanide v0.1.0, running cyan v0.1.0\n")
-
     val parser = CyanSourceParser()
 
     val runtimeSource = parser.parseToEnd(File("runtime/runtime.cy").readText())
     val runtimeFirDocument = SourceLower.lower(runtimeSource, FirDocument())
 
-    var source: CyanSource
-    val timeTakenToParse = measureTime {
-        source = parser.parseToEnd("""
+    val source = parser.parseToEnd("""
             |type Person = struct {
             |   name: str,
             |   age: i32
@@ -65,39 +52,13 @@ fun main() {
             |print(strlen("Hamza"))
             |print(powerOfTwo(5))
             """.trimMargin())
-    }
 
     val fir = SourceLower.lower(source, FirDocument(runtimeFirDocument.declaredSymbols))
-    println("symbols: ${fir.declaredSymbols}")
-
-    println("parsing source took ${timeTakenToParse.inMilliseconds} ms\n")
-
-//    val interpreter = Interpreter()
-//
-//    val timeTakenToInterpret = measureTime {
-//        interpreter.run(source)
-//    }
-//
-//    println("\ninterpreting code took ${timeTakenToInterpret.inMilliseconds} ms\n")
-
 
     // Remove externs for inserting stdlib translation into js
     runtimeFirDocument.declaredSymbols.removeIf { it is FirFunctionDeclaration && it.isExtern }
-    runtimeFirDocument.localFunctions.removeIf { it.isExtern }
 
-    var jsSource: String
-    val timeTakenToTranslate = measureTime {
-        jsSource = JsCompilerBackend(runtimeFirDocument).translateSource(fir, isRoot = true)
-    }
+    val jsSource = JsCompilerBackend(runtimeFirDocument).translateSource(fir, isRoot = true)
 
     println(jsSource)
-
-    println("\ncompiling code to js took ${timeTakenToTranslate.inMilliseconds} ms\n")
-
-    val backend = WasmCompilerBackend()
-
-    val wasmStrExp = backend.lowerExpression(FirExpression(FirDocument(), CyanStringLiteralExpression("Hello, world ! I will allocate too much !")))
-    val oneOtherExp = backend.lowerExpression(FirExpression(FirDocument(), CyanStringLiteralExpression("Test of alloc() !")))
-    println("heap: ${backend.heap.joinToString { "0x${it.toString(16)}" }}")
-    println("exprs: $wasmStrExp, $oneOtherExp")
 }
