c05f0afb73aa89e7b338f2c7b8cf3f74ae8cf426
Benjamin Dupont <benjamin.dupont.984@gmail.com>
2020-08-03 22:31:09 -0400

    feat(wasm-backend): track locals in context

--- src/cyan/compiler/codegen/wasm/WasmLoweringContext.kt
+++ src/cyan/compiler/codegen/wasm/WasmLoweringContext.kt
@@ -5,8 +5,16 @@ import cyan.compiler.fir.FirVariableDeclaration
 
 class WasmLoweringContext(override val backend: WasmCompilerBackend) : LoweringContext {
 
+    val locals = mutableMapOf<FirVariableDeclaration, Int>()
+
     var numLocals = 0
 
     val pointerForLocal = mutableMapOf<FirVariableDeclaration, Int>()
+
+    fun addLocal(declaration: FirVariableDeclaration): Int {
+        val n = this.locals.size + 1
+        this.locals[declaration] = n
+        return n
+    }
 
 }
