bc2feb0974d3dd2f5160678691dfad0aac17e53f
Benjamin Dupont <benjamin.dupont.984@gmail.com>
2020-08-13 20:24:58 -0400

    fix(ast): allow null for span in type annotation

--- src/cyan/compiler/parser/ast/types/CyanTypeAnnotation.kt
+++ src/cyan/compiler/parser/ast/types/CyanTypeAnnotation.kt
@@ -7,11 +7,11 @@ import cyan.compiler.parser.ast.expression.CyanIdentifierExpression
 
 sealed class CyanTypeAnnotation(override val span: Span? = null) : CyanItem {
 
-    class Literal(val literalType: Type.Primitive, span: Span?): CyanTypeAnnotation(span) {
+    class Literal(val literalType: Type.Primitive, span: Span? = null): CyanTypeAnnotation(span) {
         override fun toString() = literalType.toString()
     }
 
-    class Reference(val identifierExpression: CyanIdentifierExpression, span: Span?): CyanTypeAnnotation(span) {
+    class Reference(val identifierExpression: CyanIdentifierExpression, span: Span? = null): CyanTypeAnnotation(span) {
         override fun toString() = identifierExpression.value
     }
 
